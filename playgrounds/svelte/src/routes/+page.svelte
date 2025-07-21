371591db4a623833e3e98774fc827fac66769535
Puru Vijay <devpuruvj@gmail.com>
2025-07-21 17:13:08 +0530

    fix: correct interval initialization and toggle functionality in +page.svelte

--- playgrounds/svelte/src/routes/+page.svelte
+++ playgrounds/svelte/src/routes/+page.svelte
@@ -2,10 +2,14 @@
 	import { Interval } from "svelte-interval-rune";
 
   // const interval1 = new Interval(1000)
-  const interval2 = new Interval(1000, {immediate: false})
+  const interval1 = new Interval(1000, {immediate: false})
+  const interval2 = new Interval(1000)
 </script>
 
 <!-- <p>1: {interval1.tickCount}</p> -->
+<p>1: {interval1.tickCount}</p>
 <p>2: {interval2.tickCount}</p>
 
-<button onclick={() => interval2.paused ? interval2.resume(true) : interval2.pause()}>Toggle</button>
+<button onclick={() => interval1.paused ? interval1.resume(true) : interval1.pause()}>Toggle</button>
+
+<button onclick={() => interval2.duration = interval2.duration === 100 ? 1000 : 100}>Change Interval2 time</button>
