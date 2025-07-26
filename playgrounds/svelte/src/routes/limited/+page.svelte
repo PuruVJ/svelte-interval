8bbf8e1df51d0975342dc096a080c2e8aeb0845b
Puru Vijay <devpuruvj@gmail.com>
2025-07-26 17:16:19 +0530

    feat: add LimitedInterval and sync functionality with tests

---
+++ playgrounds/svelte/src/routes/limited/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { LimitedInterval, sync } from 'svelte-interval-rune';
+
+  const in1 = new LimitedInterval(100, 300);
+  const in2 = new LimitedInterval(200, 200);
+  const in3 = new LimitedInterval(300, 100);
+
+  const controller = sync(in1, in2, in3);
+
+  function start() {
+    controller.start();
+  }
+
+  function stop() {
+    controller.stop();
+  }
+</script>
+
+<p>Leader: {!!controller.leader}</p>
+
+<p>1: {in1.tickCount}</p>
+<p>2: {in2.tickCount}</p>
+<p>3: {in3.tickCount}</p>
+
+<button onclick={start}>Sync</button>
+<button onclick={stop}>Desync</button>
---
+++ playgrounds/svelte/src/routes/sync/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { Interval, sync } from 'svelte-interval-rune';
+
+  const in1 = new Interval(100);
+  const in2 = new Interval(200);
+  const in3 = new Interval(300);
+
+  const controller = sync(in1, in2, in3);
+
+  function start() {
+    controller.start();
+  }
+
+  function stop() {
+    controller.stop();
+  }
+</script>
+
+<p>Leader: {!!controller.leader}</p>
+
+<p>1: {in1.tickCount}</p>
+<p>2: {in2.tickCount}</p>
+<p>3: {in3.tickCount}</p>
+
+<button onclick={start}>Sync</button>
+<button onclick={stop}>Desync</button>
